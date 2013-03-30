b8cf2fd177fcce6a777e2986689b7f87819d3b6e
chris a <chris@whatbirdthings.com>
2013-03-30 11:14:13 -0400

    initial scrap commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'rack'
+gem 'activesupport'
+gem 'compass'
+
+group :development do
+  gem 'pry'
+  gem 'heroku'
+end
---
+++ mlb_game_data.rb
@@ -0,0 +1,41 @@
+module MLBGameData
+  BASE_URL = 'http://gd2.mlb.com/components/game/mlb/'
+  TEAMS    = {
+    ari: "Arizona Diamondbacks",
+    atl: "Atlanta Braves",
+    bal: "Baltimore Orioles",
+    bos: "Boston Red Sox",
+    chn: "Chicago Cubs",
+    cha: "Chicago White Sox",
+    cin: "Cincinnati Reds",
+    cle: "Cleveland Indians",
+    col: "Colorado Rockies",
+    det: "Detroit Tigers",
+    hou: "Houston Astros",
+    kca: "Kansas City Royals",
+    ana: "Los Angeles Angels",
+    lan: "Los Angeles Dodgers",
+    flo: "Miami Marlins",
+    mil: "Milwaukee Brewers",
+    min: "Minnesota Twins",
+    nyn: "New York Mets",
+    nya: "New York Yankees",
+    oak: "Oakland Athletics",
+    phi: "Philadelphia Phillies",
+    pit: "Pittsburgh Pirates",
+    sdn: "San Diego Padres",
+    sfn: "San Francisco Giants",
+    sea: "Seattle Mariners",
+    sln: "St. Louis Cardinals",
+    tba: "Tampa Bay Rays",
+    tex: "Texas Rangers",
+    tor: "Toronto Blue Jays",
+    was: "Washington Nationals"
+  }
+
+  def fetch(date, team, index=0)
+  end
+
+
+
+end
