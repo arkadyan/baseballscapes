de7b257cb88a5159975068650c606e7a84c46e04
the55 <chris@the55.net>
2013-03-30 12:31:43 -0400

    the data pull mostly works now

--- lib/mlb_game_data.rb
+++ lib/mlb_game_data.rb
@@ -3,8 +3,13 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-module MLBGameData
+
+
+
+class MLBGameData
   BASE_URL = 'http://gd2.mlb.com/components/game/mlb'
+  DATA_VERSION = '001'
+  CACHE_PATH = File.join(File.dirname(__FILE__), '..', 'data_cache', DATA_VERSION)
   TEAMS    = {
     ari: "Arizona Diamondbacks",
     atl: "Atlanta Braves",
@@ -38,16 +43,52 @@ module MLBGameData
     was: "Washington Nationals"
   }
 
+  attr_reader :date, :team, :index
+
+  # TODO: support double headers
+  def initialize date, team, index=0
+    @date = Date.parse date
+    @team = team.to_s.downcase
+    @index = index
+  end
+
   #useful files
   # /inning/inning_all.xml - a large file containing every pitch, etc
 
-  # TODO: support double headers
-  def fetch date, team, index=0
+  def data file = 'inning/inning_all.xml'
+    prefetched_data(file) || fetch_file(file)
   end
 
-  def self.compute_url date, team, index=0
-    date = Date.parse date
-    raise ArgumentError unless date.is_a? Date
+  def fetch_file file
+    remote_data = open(compute_url + file).read
+    file_path = compute_url.split('/').last + file.gsub('/', '-')
+
+    puts "--- fetching remote #{compute_url+file}"
+
+    open(File.join(CACHE_PATH)+'/'+file_path, 'wb') do |file|
+        begin
+          puts "--- writing #{File.join(CACHE_PATH)+file_path}"
+          file << remote_data
+        rescue
+          puts "ERROR: #{compute_url}"
+        end
+      end
+     Nokogiri::XML remote_data
+  end
+
+  def prefetched_data file
+    regex = /#{date.to_s.gsub(/-/,'_')}.*_#{team}mlb_.*#{file.sub(/\//, '-')}/
+    Dir.foreach(File.join(CACHE_PATH)) do |f|
+      if f.match regex
+        puts "--- cache hit #{f}"
+        return Nokogiri::XML(open([File.join(CACHE_PATH), f].join('/')))
+      end
+    end
+    puts "--- cache miss "
+    nil
+  end
+
+  def compute_url
     raise ArgumentError unless TEAMS.keys.include? team.to_sym
     dir_url = [
         BASE_URL,
@@ -63,4 +104,7 @@ end
 
 date = '2012-07-13'
 
-puts MLBGameData.compute_url('2012-07-13', 'lan')
+m = MLBGameData.new('2012-07-13', 'bos')
+
+a = m.data
+binding.pry
