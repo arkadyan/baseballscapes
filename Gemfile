c887df16ce98e95d20db8ecb0615319ca09a4fed
the55 <chris@the55.net>
2013-03-30 11:43:09 -0400

    working on the stats gathering

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra'
 gem 'rack'
 gem 'activesupport'
 gem 'compass'
+gem 'nokogiri'
 
 group :development do
   gem 'pry'
--- lib/mlb_game_data.rb
+++ lib/mlb_game_data.rb
@@ -1,5 +1,10 @@
+require 'date'
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 module MLBGameData
-  BASE_URL = 'http://gd2.mlb.com/components/game/mlb/'
+  BASE_URL = 'http://gd2.mlb.com/components/game/mlb'
   TEAMS    = {
     ari: "Arizona Diamondbacks",
     atl: "Atlanta Braves",
@@ -33,9 +38,29 @@ module MLBGameData
     was: "Washington Nationals"
   }
 
-  def fetch(date, team, index=0)
+  #useful files
+  # /inning/inning_all.xml - a large file containing every pitch, etc
+
+  # TODO: support double headers
+  def fetch date, team, index=0
   end
 
+  def self.compute_url date, team, index=0
+    date = Date.parse date
+    raise ArgumentError unless date.is_a? Date
+    raise ArgumentError unless TEAMS.keys.include? team.to_sym
+    dir_url = [
+        BASE_URL,
+        "/year_#{date.year}/month_#{date.strftime '%m'}/day_#{date.strftime '%d'}"
+      ].join
 
+    doc = Nokogiri::HTML(open(dir_url))
+    game = doc.search('a').map{|a| a.attr("href")}.find{|a| a.match /_#{team.to_s.downcase}mlb_/}
+    return [dir_url,game]*'/'
+  end
 
 end
+
+date = '2012-07-13'
+
+puts MLBGameData.compute_url('2012-07-13', 'lan')
